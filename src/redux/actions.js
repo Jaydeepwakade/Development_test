521b22beb67bd0015f9f823e6b5d6e336b49cd01
Jaydeepwakade <jaydeepwakade123@gmail.com>
2024-08-31 05:56:49 +0530

    all testet working as mention

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,5 +1,5 @@
 
-export const API_URL = "http://localhost:3000/details"
+export const API_URL = "http://localhost:3000/details" 
 export const FETCHDATAREQUEST = "FETCHDATAREQUEST"
 export const FETCHDATASUCCSES = "FETCHDATASUCCSES"
 export const FETCHDATAERROR = "FETCHDATAERROR"
@@ -16,6 +16,36 @@ export const fetchDatasuccses = (data) => {
 export const fetchDataerror = (e) => {
     return { type: FETCHDATAERROR, payload: e }
 }
+const fallbackData = [
+    {
+        id: 1,
+        name: "Raju Rassibomb",
+        img: "/assets/modalbanner.png",
+        otherimg: ["/assets/crackers.png"],
+        price: "200",
+        description: "This product packs an extra punch...",
+        Contents: [
+            "10 hand-hurting pieces of rassi bombs",
+            "Filled with great pain",
+            "Raju’s hopes"
+        ],
+        shipping: "Before Raju understands that his dreams do not matter."
+    },
+    {
+        id: 2,
+        name: "Raju Rassibomb",
+        img: "/assets/modalbanner.png",
+        otherimg: ["/assets/crackers.png"],
+        price: "200",
+        description: "This product packs an extra punch...",
+        Contents: [
+            "10 hand-hurting pieces of rassi bombs",
+            "Filled with great pain",
+            "Raju’s hopes"
+        ],
+        shipping: "Before Raju understands that his dreams do not matter."
+    }
+];
 
  export const fetchData = () => {
     return async (dispatch) => {
@@ -25,7 +55,7 @@ export const fetchDataerror = (e) => {
             const data = await response.json()
            dispatch( fetchDatasuccses(data))
         } catch (error) {
-          dispatch(fetchDataerror(error))
+          dispatch(fetchDatasuccses(fallbackData))
         }
     }
 }
